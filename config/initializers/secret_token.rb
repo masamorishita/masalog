2458d7f1ec205adeef5b1df139bc4221b2d3a0f8
Masa Morishita <info@masamorishita.com>
2012-11-02 08:46:14 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Masalog::Application.config.secret_token = 'dfa1c07265b8c74967cfdc439acade13512843e02d9aa57cd01849d87972d4c827eca0387da2abe637fe208edd536973b91ef60eafb21e7c3a1968c5f7934953'
